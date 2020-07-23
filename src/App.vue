063bf06f0d3c7aea6b3a4cc0d61feff8b7f3a42d
robertobirijnr <robertobirijnr@hotmail.com>
2020-07-23 11:26:15 +0000

    app-nav-bar hero section list exchage and pagination components

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,117 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <Header />
+    <HeroComponent/>
+    <section class="posts">
+      <div class="container">
+        <div class="posts-type">Latest Posts</div>
+        <ExchangeList/>
+      </div>
+    </section>
+   <Pagination/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Header from '@/components/header.vue'
+import HeroComponent from '@/components/hero.vue'
+import Pagination from '@/components/pagination.vue'
+import ExchangeList from '@/components/exhange/exchangeList.vue'
+export default {
+  name: 'app',
+  components:{
+    Header ,
+    HeroComponent,
+    Pagination,
+    ExchangeList
+  }
 }
+</script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="scss">
+  @import 'assets/styles/variables.scss';
+  @import '~bulma/bulma.sass';
+  @import 'assets/styles/main.scss';
+  .hero-section {
+    position: relative;
+  }
+  .posts {
+    padding-top: 70px;
+  }
+  .posts-type {
+    font-size: 34px;
+    margin-bottom: 10px;
+    font-weight: bold;
+  }
+  .search-lookup {
+      width: 960px;
+      margin: 0 auto;
+      background-color: #262d32;
+      padding: 20px;
+      color: white;
+      border-radius: 10px;
+  }
+  .search-lookup-wrap {
+    width: 100%;
+    z-index: 2;
+    position: absolute;
+    top: auto;
+    bottom: -42px;
+  }
+  .search-card-find {
+    width: 100%;
+    height: 180px;
+    position: relative;
+    display: block;
+    border-radius: 3px;
+    text-decoration: none;
+    box-shadow: 0 0 1px rgba(0,0,0,.05);
+    background-clip: content-box;
+    background-size: cover;
+    background-position: 50% 20%;
+    border: 1px solid rgba(0,0,0,.12);
+    -webkit-tap-highlight-color: transparent;
+    &-interest {
+      position: absolute;
+      bottom: 12px;
+      right: 12px;
+      > p {
+        font-weight: bold;
+      }
+    }
+    .title {
+      color: white;
+    }
+    .subtitle {
+      color: white;
+    }
+    &-content {
+      &-date {
+        margin: 10px;
+        width: 70px;
+        text-align: center;
+        border-radius: 50%;
+        .day {
+          display: block;
+          font-size: 21px;
+          color: white;
+          font-weight: bold;
+        }
+        .month {
+          display: block;
+          color: #ff5050;
+          font-weight: bold;
+          font-size: 23px;
+          margin-bottom: -5px;
+        }
+      }
+      &-info {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        padding: 15px;
+        width: 100%;
+      }
+    }
+  }
 </style>
